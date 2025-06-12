1a9972516a4bc9ef7a543b73267aab36adebc4c3
TecnicoMags <163574222+TecnicoMags@users.noreply.github.com>
2025-06-12 15:58:34 -0300

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+// Configuração do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDZXCaGEnBd8BujXKcrruSVFzvZDbtGToM",
+  authDomain: "web-rotas-mags.firebaseapp.com",
+  projectId: "web-rotas-mags",
+  storageBucket: "web-rotas-mags.appspot.com",
+  messagingSenderId: "59148195031",
+  appId: "1:59148195031:web:706861689db1bb7ef10c92"
+};
+
+// Inicializa o Firebase
+firebase.initializeApp(firebaseConfig);
